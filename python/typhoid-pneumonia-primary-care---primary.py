370ce88f8c822372d43aa8bb1212b49b829dc8ef
Martin Chapman <contact@martinchapman.co.uk>
2024-05-27 15:28:33 +0100

    Update made by Phenoflow

---
+++ python/typhoid-pneumonia-primary-care---primary.py
@@ -0,0 +1,25 @@
+# Eleftheria Vasileiou, Chukwuma Iwundu, Alex Williams, Clare MacRae, 2024.
+
+import sys, csv, re
+
+codes = [{"code":"3231041000006110","system":"ctv3"},{"code":"3231041000006110","system":"ctv3"}];
+REQUIRED_CODES = 1;
+with open(sys.argv[1], 'r') as file_in, open('pneumonia-primary-care-potential-cases.csv', 'w', newline='') as file_out:
+    csv_reader = csv.DictReader(file_in)
+    csv_writer = csv.DictWriter(file_out, csv_reader.fieldnames + ["typhoid-pneumonia-primary-care---primary-identified"])
+    csv_writer.writeheader();
+    codes_identified = 0;
+    for row in csv_reader:
+        newRow = row.copy();
+        for cell in row:
+            # Iterate cell lists (e.g. codes)
+            for item in re.findall(r'\(([^,]*)\,', row[cell]):
+                if(item in list(map(lambda code: code['code'], codes))): codes_identified+=1;
+                if(codes_identified>=REQUIRED_CODES):
+                    newRow["typhoid-pneumonia-primary-care---primary-identified"] = "CASE";
+                    break;
+            if(codes_identified>=REQUIRED_CODES): break;
+        if(codes_identified<REQUIRED_CODES):
+            newRow["typhoid-pneumonia-primary-care---primary-identified"] = "UNK";
+        codes_identified=0;
+        csv_writer.writerow(newRow)
